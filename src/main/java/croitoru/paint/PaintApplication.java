0a8cab6a91238795949e9b2d199ebeefaec38926
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-04-06 11:54:20 -0400

    completed paint app basics

--- src/main/java/croitoru/paint/PaintApplication.java
+++ src/main/java/croitoru/paint/PaintApplication.java
@@ -1,4 +1,24 @@
 package croitoru.paint;
 
-public class PaintApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PaintApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/paint_application.fxml"));
+
+        Scene scene = new Scene(root, 800, 400);
+
+        stage.setTitle("Paint Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/croitoru/paint/PaintController.java
+++ src/main/java/croitoru/paint/PaintController.java
@@ -1,4 +1,41 @@
 package croitoru.paint;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
+
 public class PaintController {
+
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private TextField brushSize;
+    @FXML
+    private CheckBox eraser;
+
+    public void initialize(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        canvas.setOnMouseDragged(e -> {
+            double size = Double.parseDouble(brushSize.getText());
+            double x = e.getX() - size / 2;
+            double y = e.getY() - size / 2;
+
+            if (eraser.isSelected()){
+                gc.clearRect(x,y,size,size);
+            }else{
+                gc.setFill(colorPicker.getValue());
+                gc.fillRect(x,y,size,size);
+            }
+        });
+    }
+    public void onExit(){
+        Platform.exit();
+    }
 }
